a073f548798240c8ee4aaeb2f6a930b3c996b7c1
RisingJade <csm48173738@gmail.com>
2017-04-10 17:41:42 +0900

    spwan basic GUI form

--- src/App.java
+++ src/App.java
@@ -1,5 +1,41 @@
+import javax.swing.*;
+
 /**
  * Created by csm26 on 2017-04-10.
  */
 public class App {
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Make sure we have nice window decorations.
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        frame.setSize(432,700);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+
+            }
+        });
+    }
 }
