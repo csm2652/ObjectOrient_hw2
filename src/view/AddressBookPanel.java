aa9ef5df046d1f169e086404c16c6a71a1564dbd
clucle <wjdenwls123@gmail.com>
2017-04-12 01:48:55 +0900

    feat: update AddressBookPanel basic

--- src/view/AddressBookPanel.java
+++ src/view/AddressBookPanel.java
@@ -1,12 +1,50 @@
 package view;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
+import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS;
+
 public class AddressBookPanel extends JPanel {
-    Button button = new Button("전화번호부");
+    // Tool Panel Bar
+    private JPanel jAddressBookBarPanel;
+
+    // Scroll Panel
+    public JScrollPane jAddressBookPane;
+
     public AddressBookPanel() {
-        add(button);
+        showAddressBookBarPanel();
+        showAddressBookPane();
         setVisible(true);
     }
+
+    private void showAddressBookBarPanel() {
+        // init Panel
+        jAddressBookBarPanel = new JPanel();
+        jAddressBookBarPanel.setBackground(new Color(23,169,146));
+        jAddressBookBarPanel.setPreferredSize(new Dimension(420, 64));
+
+        JButton btnAddCall = new JButton("");
+        //btnAddCall.addActionListener(new RecentCallPanel.AddCallBtnListener());
+        ImageIcon iconAddCall = new ImageIcon("src\\resource\\images\\img_add_call.png");
+        btnAddCall.setIcon(iconAddCall);
+        btnAddCall.setBorder(new EmptyBorder(0, 0, 0, 0));
+
+        JButton btnDelCall = new JButton("");
+        //btnDelCall.addActionListener(new RecentCallPanel.DelCallBtnListener());
+        ImageIcon iconDelCall = new ImageIcon("src\\resource\\images\\img_del.png");
+        btnDelCall.setIcon(iconDelCall);
+        btnDelCall.setBorder(new EmptyBorder(0, 0, 0, 0));
+
+        add(jAddressBookBarPanel, "North");
+        setVisible(true);
+    }
+
+    private void showAddressBookPane() {
+        jAddressBookPane = new JScrollPane();
+        jAddressBookPane.setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_ALWAYS);
+        jAddressBookPane.setSize(418, 600);
+    }
+
 }
